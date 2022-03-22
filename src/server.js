be5ea87272f8f87c840a0b5418fdf336ac6a59f0
pablo <pablords@gmail.com>
2022-03-21 22:34:17 -0300

    *

--- src/server.js
+++ src/server.js
@@ -1,32 +1,53 @@
 import http from "http"
 import { Readable } from "stream"
 import { randomUUID } from "crypto"
+import axios from "axios"
+import fs from "fs"
+
+const getLog = async () => {
+
+    for (var i = 0; i < 10; i++) {
+        (async function (i) {
+            setTimeout(async function () {
+                const { data, status } = await axios.get("http://dev.jenkins.com.br/job/kubeconfig/11/logText/progressiveText?start=0")
+                const fileStream = fs.createWriteStream('./log.txt')
+                if (status == 200) {
+                    fileStream.write(String(data));
+                }
+
+            }, 5000 * i);
+            // const index = data.search("SUCCESS")
+            // if (index) return
+        })(i);
+    };
+}
+
+
 
 //function generator a medida que processa retorna para quem chamou
-
 function* run() {
-    
-    for (let index = 0; index < 99; index++) {
-        
-        const data = {
-            id: randomUUID(),
-            name: `Pablo-${index}`
-        }
 
-        setTimeout(() => {
-            console.log(data);
-          }, 1000 * index);
+
+    for (let index = 0; index < 10; index++) {
+        // const data = {
+        //     id: randomUUID(),
+        //     name: `Pablo-${index}`
+        // }
+
+        const file = fs.readFileSync("./log.txt", { encoding: "utf8" });
+
         // ele nao aguarda o for terminar. a medida que processa ja passa para frente
-        yield data
+        yield file
 
     }
 }
 
 async function handler(req, res) {
+    getLog()
     const readable = new Readable({
         read() {
             for (const data of run()) {
-                console.log(`sending`, data)
+                console.log(`sending`, JSON.stringify(data))
                 this.push(JSON.stringify(data) + "\n")
             }
             this.push(null)
